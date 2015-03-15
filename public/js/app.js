5a76927661ee8a803992b64dde3fd21ba1558253
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-03-15 12:00:58 -0400

    Enhanced mobile appearance
    Fix graph appearance in mobile

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,9 @@ function drawChart(data) {
     legend: { position: 'right' },
     interpolateNulls: true,
     hAxis: { slantedText: true},
-    vAxis: { viewWindow: { min: 0, max: 10}, ticks: [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10] }
+    vAxis: { viewWindow: { min: 0, max: 10}, ticks: [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10] },
+    width: $("#curve_chart").width(),
+    height: $("#curve_chart").height()
   };
 
   var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
